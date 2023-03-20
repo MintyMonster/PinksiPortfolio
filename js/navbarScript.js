765b22d33b912092bef07a2478160580bab12210
MintyMonster <camerongliddell@gmail.com>
2023-03-20 18:01:23 +0000

    Mobile compatability

---
+++ js/navbarScript.js
@@ -0,0 +1,7 @@
+function moveNavIn() {
+    document.getElementById("navbar-responsive").style.transform = 'translate(-100vw)';
+}
+
+function moveNavOut(){  
+    document.getElementById("navbar-responsive").style.transform = 'translate(0vw)';
+}
